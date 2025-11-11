e74fa3ac382508e744d106f4d14b0111cd068b83
rwilliams175 <rwilliams175@phm.k12.in.us>
2025-11-11 11:09:04 -0500

    Update mnist_tut1.py

--- mnist_tut1.py
+++ mnist_tut1.py
@@ -6,6 +6,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import random
+import keras
+from keras.datasets import cifar10
+from keras.utils import to_categorical
 #print out versions of libaries
 print("Tensorflow: ", tf.__version__ , " Seaborn:" , sns.__version__)
 
@@ -13,10 +16,10 @@ print("Tensorflow: ", tf.__version__ , " Seaborn:" , sns.__version__)
 mnist = tf.keras.datasets.mnist
 #create the training and testing data frames
 #(training images, training labels), (testing images, testing labels)
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
 #show the number of examples in each labeled set
 
-sns.countplot(x=y_train)
+#sns.countplot(x=y_train)
 #show the plot!
 plt.show()
 
@@ -25,20 +28,19 @@ print("Any NaN Training:", np.isnan(x_train).any())
 print("Any NaN Testing: ", np.isnan(x_test).any())
 
 #tell the model what shape to expect
-input_shape = (28, 28, 1) #28x28 p0x, 1 color channel (grayscale) - 3 for RGB
+input_shape = (32, 32, 3) #28x28 p0x, 1 color channel (grayscale) - 3 for RGB
 
 #reshape the training and testing data
-x_train = x_train.reshape(x_train.shape[0], x_train.shape[1], x_train.shape[2], 1)
+x_train = x_train.astype('float32')
 x_train = x_train/255.0 #normalize the data to be between 0 and 1
 #same for testing
-x_test = x_test.reshape(x_test.shape[0], x_test.shape[1], x_test.shape[2], 1)
+x_test = x_test.astype('float32')
 x_test = x_test/255.0 #normalize the data to be between 0 and 1
 #convert our labels to be one-hot, not sparce
-y_train = tf.one_hot(y_train.astype(np.int32), depth = 10)
-y_test = tf.one_hot(y_test.astype(np.int32), depth = 10)
-
+y_train = to_categorical(y_train, 10)
+y_test = to_categorical(y_test, 10)
 #show an example image from MNIST
-plt.imshow(x_train[random.randint(0,59999)][:,:,0], cmap='grey')
+plt.imshow(x_train[random.randint(0,59999)][:,:,0])
 plt.show()
 
 
@@ -50,18 +52,19 @@ epochs = 5
 model = tf.keras.models.Sequential(
     [
         tf.keras.layers.Conv2D(32, (5,5), padding = 'same', activation = 'relu', input_shape = input_shape),
-        tf.keras.layers.Conv2D(32, (3,3), padding = 'same', activation = 'relu', input_shape = input_shape),
         tf.keras.layers.MaxPool2D(),
         tf.keras.layers.Dropout(0.25),
         tf.keras.layers.Conv2D(64, (3,3), padding = 'same', activation = 'relu', input_shape = input_shape),
         tf.keras.layers.Conv2D(64, (3,3), padding = 'same', activation = 'relu', input_shape = input_shape),
         tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(128, activation = 'relu'),
+        tf.keras.layers.Dropout(0.35),
         tf.keras.layers.Dense(num_classes, activation = 'softmax')
     ]
 )
 
-model.compile(optimizer=tf.keras.optimizers.RMSprop(epsilon=1e-08), loss='categorical_crossentropy', metrics=['acc'])
-history = model.fit(x_train, y_train, batch_size = batch_size, epochs = epochs, validation_data = (x_test, y_test))
+model.compile(optimizer='adam',loss='categorical_crossentropy', metrics=['acc'])
+history = model.fit(x_train, y_train,epochs=10,validation_split=0.1)
 
 fig, ax = plt.subplots(2,1)
 ax[0].plot(history.history['loss'], color = 'b', label = 'Training Loss')
